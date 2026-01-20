f5280de52788259c7d0fcdc052261a59c9c0baf0
Alan Wynn <alan.wynn@friendlydev.com>
2026-01-20 13:46:03 +0000

    Modify package metadata in setup.py
    Updated version, description, URL, and author for the package.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='github-backup',
-    version='1.0.0',
-    description='',
-    url='https://github.com/lnxd/docker-github-backup',
-    author='lnxd',
+    version='0.0.3',
+    description='Backup GitHub Repos Locally using GitHub Access Tokens',
+    url='https://github.com/djekl/docker-github-backup',
+    author='djekl',
     install_requires=['requests'],
     scripts=['github-backup.py'],
     zip_safe=True
